099aea83c6169976fcc2f1bb43d7cb97202e838a
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-03-23 20:43:28 +0800

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -1,29 +1,40 @@
 class BankAccount:
     def __init__(self, balance=0):
-        if check_neg(balance):
+        if balance < 0:
+            raise ValueError(f"Cannot open account with {balance} balance")
+        else:
             self.balance = balance
 
     def deposit(self, amount):
-        if check_neg(amount):
+        if amount < 0:
+            raise ValueError(f"Cannot deposit {amount}")
+        else:
             self.balance += amount
 
     def withdraw(self, amount):
-        if check_neg(amount):
+        if amount < 0:
+            raise ValueError(f"Cannot withdraw {amount}")
+        elif amount > self.balance:
+            raise ValueError(f"Can't withdraw {amount} with {self.balance} balance")
+        else:
             self.balance -= amount
 
     def transfer(self, bank2, amount):
-        if check_neg(amount):
+        if amount < 0:
+            raise ValueError(f"Cannot withdraw {amount}")
+        elif amount > self.balance:
+            raise ValueError(f"Can't withdraw {amount} with {self.balance} balance")
+        else:
             self.balance -= amount
             bank2.balance += amount
 
-
-def check_neg(number):
-    if number < 0:
-        return False
+    def __repr__(self):
+        return f"BankAccount(balance={self.balance})"
 
 
 a1 = BankAccount()
 print(a1.balance)
+print(a1)
 a1.deposit(10)
 print(a1.balance)
 a2 = BankAccount(balance=20)
@@ -33,3 +44,4 @@ print(a2.balance)
 a1.transfer(a2, 3)
 print(a1.balance)
 print(a2.balance)
+print(a2)
