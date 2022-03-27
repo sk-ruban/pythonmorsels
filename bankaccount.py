cac867f1d75c83a0e1d089ce46b0c3c902353cf3
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-03-28 02:07:31 +0800

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -1,47 +1,56 @@
 class BankAccount:
+    number = 1
+    accounts = []
+
     def __init__(self, balance=0):
         if balance < 0:
-            raise ValueError(f"Cannot open account with {balance} balance")
+            # Bonus 1 - Raise ValueError for negative checks
+            raise ValueError(f"Can't open account with {balance} balance")
         else:
-            self.balance = balance
+            self._balance = balance
+        # Bonus 2 - Assign unique account number (Differentiate Class & Object - self)
+        self.number = BankAccount.number
+        BankAccount.number += 1
+        BankAccount.accounts.append(self)
+
+    # Bonus 3 - @property creates a read-only attribute (Remember to _balance the rest)
+    @property
+    def balance(self):
+        return self._balance
 
     def deposit(self, amount):
         if amount < 0:
-            raise ValueError(f"Cannot deposit {amount}")
+            raise ValueError(f"Can't deposit {amount}")
         else:
-            self.balance += amount
+            self._balance += amount
 
     def withdraw(self, amount):
         if amount < 0:
-            raise ValueError(f"Cannot withdraw {amount}")
+            raise ValueError(f"Can't withdraw {amount}")
         elif amount > self.balance:
             raise ValueError(f"Can't withdraw {amount} with {self.balance} balance")
         else:
-            self.balance -= amount
+            self._balance -= amount
 
     def transfer(self, bank2, amount):
         if amount < 0:
-            raise ValueError(f"Cannot withdraw {amount}")
-        elif amount > self.balance:
+            raise ValueError(f"Can't withdraw {amount}")
+        elif amount > self._balance:
             raise ValueError(f"Can't withdraw {amount} with {self.balance} balance")
         else:
-            self.balance -= amount
-            bank2.balance += amount
+            self._balance -= amount
+            bank2._balance += amount
 
     def __repr__(self):
         return f"BankAccount(balance={self.balance})"
 
 
-a1 = BankAccount()
-print(a1.balance)
-print(a1)
-a1.deposit(10)
-print(a1.balance)
-a2 = BankAccount(balance=20)
-print(a2.balance)
-a2.withdraw(15)
-print(a2.balance)
-a1.transfer(a2, 3)
-print(a1.balance)
-print(a2.balance)
-print(a2)
+account1 = BankAccount()
+print(BankAccount.accounts)
+account2 = BankAccount(100)
+print(BankAccount.accounts)
+print(account1.number)
+print(account2.number)
+account1.deposit(400)
+print(account1)
+account1.transfer(account2, 20)
