8dafcf7e88bf9ea844170544f5b92fbbfc6728f8
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-04-05 14:58:57 +0800

    Update uniques_only.py

--- uniques_only.py
+++ uniques_only.py
@@ -1,11 +1,25 @@
+# Original Solution - Too slow as not in list requires looping through whole list!
 def uniques_only(iterable):
-    for key in dict.fromkeys(iterable):
-        print(key)
+    checklist = []
+    for item in iterable:
+        if item not in checklist:
+            checklist.append(item)
+            # Bonus 1
+            yield item
+"""
+# Cannot use set(iterable) as it is unordered
+# However, Sets rely on hashes for lookups -> Much faster
 
-
+def uniques_only(iterable):
+    seen = set()
+    for item in iterable:
+        if item not in seen:
+            yield item
+            seen.add(item)
+"""
 uniques_only([1, 2, 2, 1, 1, 3, 2, 1])
 nums = [1, -3, 2, 3, -1]
 squares = (n**2 for n in nums)
 uniques_only(squares)
 nums = iter([1, 2, 3])
-uniques_only(nums)
+output = uniques_only(nums)
