33c4b7a67de1178aa14bb8f304a1f4c11bfb5f98
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-04-20 18:00:17 +0800

    Update NextDate.py

--- NextDate.py
+++ NextDate.py
@@ -4,25 +4,26 @@ from datetime import date
 
 class NextDate:
 
+    """Answers questions about the next Monday/Tuesday/etc."""
+
     def __init__(self, day, *, after_today=False):
         self.day = day
         self.after_today = after_today
 
     def days_until(self):
-        if self.after_today:
-            print(self.day - date.today().weekday() % 7 if 0 else 7)
-            return self.day - date.today().weekday() % 7 if 0 else 7
+        # Calculates the days difference mod 7
+        days_difference = (self.day - date.today().weekday()) % 7
+        if self.after_today & (days_difference == 0):
+            return 7
         else:
-            print((self.day - date.today().weekday()) % 7)
-            return (self.day - date.today().weekday()) % 7
+            return days_difference
 
     def date(self):
-        print(date.today() + datetime.timedelta(self.days_until()))
         return date.today() + datetime.timedelta(self.days_until())
 
     def __repr__(self):
         if self.after_today:
-            return f"NextDate(Weekday.{self.day}2)"
+            return f"NextDate(Weekday.{self.day}, after_today=True)"
         else:
             return f"NextDate(Weekday.{self.day})"
 
@@ -38,45 +39,38 @@ class Weekday:
 
 
 def next_date(day, after_today=False):
-    print(date.today() + datetime.timedelta(days_until(day, after_today)))
+    return date.today() + datetime.timedelta(days_until(day, after_today))
 
 
 def days_until(day, after_today=False):
-    if after_today:
-        print(day - date.today().weekday() % 7 if 0 else 7)
-        return day - date.today().weekday() % 7 if 0 else 7
+    days_difference = (day - date.today().weekday()) % 7
+    if after_today & (days_difference == 0):
+        return 7
     else:
-        print((day - date.today().weekday()) % 7)
-        return (day - date.today().weekday()) % 7
+        return days_difference
 
 
 def next_tuesday(after_today=False):
-    print(date.today() + datetime.timedelta(days_to_tuesday(after_today)))
+    return next_date(Weekday.TUESDAY, after_today)
 
 
 def days_to_tuesday(after_today=False):
-    if after_today:
-        print(Weekday.TUESDAY - date.today().weekday())
-        print(Weekday.TUESDAY - date.today().weekday() % 7 if 0 else 7)
-        return Weekday.TUESDAY - date.today().weekday() % 7 if 0 else 7
-    else:
-        print((Weekday.TUESDAY - date.today().weekday()) % 7)
-        return (Weekday.TUESDAY - date.today().weekday()) % 7
+    return days_until(Weekday.TUESDAY, after_today)
 
 
-# next_thursday = NextDate(Weekday.THURSDAY)
-# next_thursday.days_until()
-# next_thursday.date()
-# next_friday = NextDate(Weekday.FRIDAY)
-# next_friday.days_until()
-# next_friday.date()
-# next_friday = NextDate(Weekday.THURSDAY, after_today=True)
-# next_friday.days_until()
-# next_friday.date()
+next_thursday = NextDate(Weekday.THURSDAY)
+next_thursday.days_until()
+next_thursday.date()
+next_friday = NextDate(Weekday.FRIDAY)
+next_friday.days_until()
+next_friday.date()
+next_friday = NextDate(Weekday.THURSDAY, after_today=True)
+next_friday.days_until()
+next_friday.date()
 
 # Bonus 3
-# next_date(Weekday.FRIDAY)
-# days_until(Weekday.FRIDAY)
+next_date(Weekday.FRIDAY)
+days_until(Weekday.FRIDAY)
 next_tuesday(after_today=True)
 days_to_tuesday()
 
