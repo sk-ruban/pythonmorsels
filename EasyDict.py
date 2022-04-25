d8f1d628c3137ffe67cd8c8cb5418c12cc2c5942
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-04-25 16:52:53 +0800

    Update EasyDict

--- EasyDict.py
+++ EasyDict.py
@@ -2,16 +2,29 @@ class EasyDict:
 
     """Accepts a Dict and creates an instance which can work with key & attribute lookups"""
 
-    def __init__(self, input_dict={}):
+    # Use **kwargs for accepting optional key-word arguments
+    def __init__(self, input_dict = {}, **kwargs):
         self.__dict__.update(input_dict)
+        self.__dict__.update(kwargs)
 
-    # Support key lookups
-    def __getitem__(self, index):
-        return self.__dict__[index]
+    # Support key & index lookups
+    def __getitem__(self, key):
+        return self.__dict__[key]
 
     # Support key & attribute assignment
     def __setitem__(self, key, value):
         self.__dict__[key] = value
+
+    '''
+    # Trey's Solution
+    # To Test for equality
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    # Does the same thing as __getitem__, except it provides a default value
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+    '''
 
 
 person = EasyDict({'name': "Trey Hunner", 'location': "San Diego"})
@@ -19,3 +32,6 @@ print(person.name)
 print(person['location'])
 person.location = "Portland"
 print(person['location'])
+
+person = EasyDict(name="Trey Hunner", location="San Diego")
+print(person.location)
