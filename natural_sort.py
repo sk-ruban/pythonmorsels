c94ae5196026f4243c2f45df085542916281d55e
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-04-27 18:23:12 +0800

    Update natural_sort.py

--- natural_sort.py
+++ natural_sort.py
@@ -1,25 +1,37 @@
-natural_key = str.casefold
+import re
+
+def natural_key(text):
+    return text.casefold()
 
 
-def natural_sort(words, key=natural_key, reverse=False):
-    post_sort = []
-    for word in sorted(words, key=key, reverse=reverse):
-        post_sort.append(word)
-    print(post_sort)
-    return post_sort
+def natural_sort(data, key=natural_key, reverse=False):
+    print(sorted(data, key=key, reverse=reverse))
+    return sorted(data, key=key, reverse=reverse)
 
+'''
+# Trey's Solution
 
-# natural_sort(['uncle', 'Yankee', 'India', 'hotel', 'zebra', 'Oscar'])
-# natural_sort(['McDonald', 'MCDONALD', 'Mcdonald', 'MacDonald'])
+def natural_key(string):
+    """Return case-normalized sorting key with numbers as integers."""
+    return tuple(
+        int(s) if s.isdigit() else s
+        for s in re.split(r'(\d+)', string.lower())
+    )
+'''
+# Base Problem
+natural_sort(['uncle', 'Yankee', 'India', 'hotel', 'zebra', 'Oscar'])
+natural_sort(['McDonald', 'MCDONALD', 'Mcdonald', 'MacDonald'])
 
 
 # Bonus 1
-# def reverse_name(name):
-#     """Key function to sort by last name first."""
-#     first, last = name.rsplit(' ', 1)
-#     return natural_key(last + ' ' + first)
-# names = ['Sarah Clarke', 'Sara Hillard', 'Sarah Chiu']
-# natural_sort(names, key=reverse_name)
-# natural_sort(names)
+def reverse_name(name):
+    """Key function to sort by last name first."""
+    first, last = name.rsplit(' ', 1)
+    return natural_key(last + ' ' + first)
+names = ['Sarah Clarke', 'Sara Hillard', 'Sarah Chiu']
+natural_sort(names, key=reverse_name)
+natural_sort(names)
 
+# Bonus 2
 natural_sort(['my 13 cats', 'your 1 pig', 'my 2 dogs', 'your 16 squirrels'])
+
