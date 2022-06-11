05ee701876a5b7d4519905e31c42836365c6291c
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-06-11 19:44:43 +0100

    Update int_to_roman.py

--- int_to_roman.py
+++ int_to_roman.py
@@ -1,34 +1,56 @@
-roman_dict = [
-    (1000, "M"),
-    (900, "CM"),
-    (500, "D"),
-    (400, "CD"),
-    (100, "C"),
-    (90, "XC"),
-    (50, "L"),
-    (40, "XL"),
-    (10, "X"),
-    (9, "IX"),
-    (5, "V"),
-    (4, "IV"),
-    (1, "I"),
-]
+convert_table = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I'
+}
 
 
-def int_to_roman(number):
-    result = ""
-    for (hindu, roman) in roman_dict:
-        # takes two numbers and returns a pair of numbers (a tuple) of quotient and remainder
-        (factor, number) = divmod(number, hindu)
-        result += roman * factor
+def int_to_roman(num):
+    roman = []
+    # .items() to return the k,v of a dict
+    for k, v in convert_table.items():
+        # // floor division - return w/o remainder
+        roman.append(num//k * v)
+        # % returns the remainder
+        num %= k
+    # "".join() - joins elements of an iterable together
+    return "".join(roman)
+
+
+def roman_to_int(num):
+    ix = 0
+    result = 0
+    while ix < len(num):
+        for k, v in convert_table.items():
+            if num.startswith(v, ix):
+                result += k
+                ix += len(v)
+                break
+        else:
+            raise ValueError('Invalid Roman number.')
     return result
 
 
-def roman_to_int(number):
-    result = ""
-
-
+# Base + Bonus 1
 int_to_roman(5)
-int_to_roman(8)
-int_to_roman(14)
-roman_to_int('IIII')
+int_to_roman(4)
+int_to_roman(9)
+
+# Bonus 2
+# roman_to_int('I')
+# roman_to_int('MDCCLXXXVI')
+# roman_to_int("XVA")
+
+# Bonus 3
+roman_to_int('IV')
+roman_to_int('MCMXLVIII')
