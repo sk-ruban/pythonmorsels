27c9877e137b339df72b4bf3e4a7757fc0357176
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-07-13 20:53:15 +0800

    Update total_length.py

--- total_length.py
+++ total_length.py
@@ -1,11 +1,19 @@
-def total_length(*objects):
+import operator
+
+
+def total_length(*obj, use_hints=False):
     length = 0
-    for each in objects:
-        if hasattr(each, '__len__'):
-            length += len(each)
+    for each in obj:
+        if use_hints is True:
+            length += operator.length_hint(each)
         else:
-            length += sum(1 for _ in each)
+            if hasattr(each, '__len__'):
+                length += len(each)
+            else:
+                length += sum(1 for _ in each)
+    print(length)
     return length
+
 
 # Base Problem
 total_length([4, 5], (6, 7))
@@ -18,3 +26,7 @@ total_length(n for n in numbers if n % 2 == 0)
 
 # Bonus 2
 total_length(range(1000), range(1000000000))
+
+# Bonus 3
+total_length(reversed(range(1000000000)), use_hints=True)
+total_length([100, 50, 20, 30, 10], 0, [100, 50, 20, 30, 10], 0, use_hints=True)
