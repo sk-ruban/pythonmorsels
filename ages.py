4bd4b63aaa84e0dc56da833ea3a7ab4eb1dd5977
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-07-14 18:10:56 +0800

    Update ages.py

--- ages.py
+++ ages.py
@@ -1,13 +1,48 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+
+# def is_over(limit, birthday):
+#     birthday_formatted = datetime.fromisoformat(birthday)
+#     today = datetime(3000,2,28)
+#     if birthday_formatted + relativedelta(years=limit) < today:
+#         print(birthday_formatted.replace(year=birthday_formatted.year + limit))
+#         print(birthday_formatted + relativedelta(years=limit))
+#         print("True")
+#         return True
+#     else:
+#         print(birthday_formatted + relativedelta(years=limit))
+#         print("False")
+#         return False
 
 def is_over(limit, birthday):
+
+    # Return True if person is above a certain age
+
     birthday_formatted = datetime.fromisoformat(birthday)
-
-    if birthday_formatted + timedelta(days=limit*365) < datetime.today():
-        return True
+    today = datetime(2000, 3, 16)
+    if birthday_formatted.year + limit > today.year:
+        print("False1")
+    elif birthday_formatted.year + limit == today.year:
+        if birthday_formatted.month > today.month:
+            print("False2")
+        elif birthday_formatted.month == today.month:
+            if birthday_formatted.day > today.day:
+                print("False3")
+            else:
+                print("True")
+        else:
+            print("True")
     else:
-        return False
+        print("True")
+    
 
+# Base Problem
+is_over(18, '2982-03-15')
+is_over(18, '2982-02-15')
+is_over(18, '2990-04-15')
 
-is_over(18, '1982-03-15')
+# Bonus 1
+is_over(16, '2984-02-29')
+is_over(16, '2984-02-27')
+is_over(16, '2984-03-01')
