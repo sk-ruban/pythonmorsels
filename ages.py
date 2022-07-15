5817e76e3baf1a7bac0529c6adb71fc70ad8c192
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-07-15 14:56:49 +0800

    My solutions up to Bonus 2

--- ages.py
+++ ages.py
@@ -1,41 +1,55 @@
 from datetime import datetime
+
+'''
 from dateutil.relativedelta import relativedelta
 
-
-# def is_over(limit, birthday):
-#     birthday_formatted = datetime.fromisoformat(birthday)
-#     today = datetime(3000,2,28)
-#     if birthday_formatted + relativedelta(years=limit) < today:
-#         print(birthday_formatted.replace(year=birthday_formatted.year + limit))
-#         print(birthday_formatted + relativedelta(years=limit))
-#         print("True")
-#         return True
-#     else:
-#         print(birthday_formatted + relativedelta(years=limit))
-#         print("False")
-#         return False
+# Unable to use relativedelta as PythonMorsels does not accept dateutil
 
 def is_over(limit, birthday):
 
-    # Return True if person is above a certain age
+    """Return True if person is above a certain age"""
+    
+    birthday_formatted = datetime.fromisoformat(birthday)
+    today = datetime(3000,2,28)
+    if birthday_formatted + relativedelta(years=limit) < today:
+        return True
+    else:
+        return False
+'''
+
+
+def is_over(limit, birthday):
+
+    """Return True if person is above a certain age"""
 
     birthday_formatted = datetime.fromisoformat(birthday)
-    today = datetime(2000, 3, 16)
+    today = datetime(2016, 2, 29)
     if birthday_formatted.year + limit > today.year:
-        print("False1")
+        return False
     elif birthday_formatted.year + limit == today.year:
         if birthday_formatted.month > today.month:
-            print("False2")
+            return False
         elif birthday_formatted.month == today.month:
             if birthday_formatted.day > today.day:
-                print("False3")
+                return False
             else:
-                print("True")
+                return True
         else:
-            print("True")
+            return True
     else:
-        print("True")
-    
+        return True
+
+
+def get_age(birthday, exact=False):
+
+    """Return a person's current age given their birthdate"""
+
+    counter = 0
+    for age in range(1, 1000):
+        if is_over(age, birthday):
+            counter = age
+    print(counter)
+
 
 # Base Problem
 is_over(18, '2982-03-15')
@@ -46,3 +60,14 @@ is_over(18, '2990-04-15')
 is_over(16, '2984-02-29')
 is_over(16, '2984-02-27')
 is_over(16, '2984-03-01')
+
+# Bonus 2
+get_age('1942-01-08')
+get_age('1815-02-28')
+get_age('2980-02-28')
+get_age('2980-08-28')
+
+# Bonus 3
+get_age('2984-02-29', exact=True)
+float(get_age('2984-02-29', exact=True))
+float(get_age('2984-02-27', exact=True))
